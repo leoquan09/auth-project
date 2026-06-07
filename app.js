c390eb1a7a9365695c9780bb6fdf94fd461097fb
Leo <leoquan09@gmail.com>
2026-06-07 13:25:38 -0700

    set up all the stuff that is need for now

---
+++ app.js
@@ -0,0 +1,33 @@
+const path = require('node:path');
+const { Pool } = require('pg');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const pool = new Pool({
+    user: 'leo',
+    host: 'localhost',
+    database: 'auth_project',
+    password: '09272011',
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 30000
+});
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000, (error) => {
+    if(error) {
+        throw error;
+    };
+    console.log('app is up and running on localhost:3000');
+});
